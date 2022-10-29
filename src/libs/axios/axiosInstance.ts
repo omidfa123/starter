7b45df5916d4b308472be3cdb0b34a4cb46e9f9d
omidfa <omidfa1234@gmail.com>
2022-10-29 11:54:14 +0330

    change to https

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -1,14 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const baseUrl = 'http://ms2.atramart.com/api/v1';
+const baseUrl = 'https://ms2.atramart.com/api/v1';
 
 const config: AxiosRequestConfig = {
   baseURL: baseUrl,
-  // headers: {
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Credentials': true,
-  //   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  // },
 };
 
 export const axiosInstance = axios.create(config);
