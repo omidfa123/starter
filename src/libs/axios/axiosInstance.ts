f6b449bc7bcc44bdb5024896fcd8943c23b999b4
omidfa <omidfa1234@gmail.com>
2022-10-29 10:58:04 +0330

    update .gitlab

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -4,11 +4,11 @@ const baseUrl = 'http://ms2.atramart.com/api/v1';
 
 const config: AxiosRequestConfig = {
   baseURL: baseUrl,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Credentials': true,
+  //   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  // },
 };
 
 export const axiosInstance = axios.create(config);
