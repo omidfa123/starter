6ea55d6a1d568e3ad684b46bde18664756bb21ce
omidfa <omidfa1234@gmail.com>
2023-06-22 22:55:53 +0330

    fix  type errors of layout

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,13 +1,15 @@
 import Footer from "../components/footer";
 import Header from "../components/header-pc";
 
-export default function ShopLayout(p) {
-  console.log(p);
-
+export default function ShopLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main className="flex min-h-screen flex-col bg-body-gradient">
       <Header />
-      <main className="flex-1">{p.children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </main>
   );
